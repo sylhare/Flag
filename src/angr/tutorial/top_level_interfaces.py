c95e8b4725bf2152ff1cf8618ad165bc10ac4db2
sylhare <sylhare@outlook.com>
2019-03-05 15:36:33 -0500

    Add Blocks

--- src/angr/tutorial/top_level_interfaces.py
+++ src/angr/tutorial/top_level_interfaces.py
@@ -3,6 +3,7 @@ https://docs.angr.io/core-concepts/toplevel
 """
 import angr
 import logging
+
 logging.getLogger('cle').setLevel('ERROR')
 
 # Create Angr project
@@ -13,11 +14,13 @@ proj = angr.Project('./bin/true')
 assert proj.entry == int(hex(proj.entry), 16)
 
 # Basic properties
+print("\n\n Basic Properties:\n")
 print(proj.arch)  # architecture the program is compiled with
 print(hex(proj.entry))  # entry is the entry point of the binary
 print(proj.filename)  # is the absolute filename of the binary
 
 # Loader
+print("\n\n Loader:\n")
 print(proj.loader)  # Loader which has the virtual space address of the binary
 print(proj.loader.shared_objects)
 print(hex(proj.loader.min_addr))
@@ -27,7 +30,15 @@ print(proj.loader.main_object)  # we've loaded several binaries into this projec
 print(proj.loader.main_object.execstack)  # Does this binary have an executable stack?
 print(proj.loader.main_object.pic)  # Is this binary position-independent?
 
-
+# Factory
+# Blocks
+print("\n\n Blocks:\n")
+block = proj.factory.block(proj.entry) # lift a block of code from the program's entry point)
+block.pp()  # pretty-print a disassembly to stdout
+print(block.instructions)  # how many instructions are there?
+print(list(hex(x) for x in block.instruction_addrs))  # what are the addresses of the instructions?
+# print(block.capstone)  # capstone disassembly
+# print(block.vex)  # VEX IRSB (that's a python internal address, not a program address)
 
 if __name__ == '__main__':
     pass
