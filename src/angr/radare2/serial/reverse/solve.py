4ba3b8412c6c7d29daceb80d5e337ae7284e89a2
sylhare <sylhare@outlook.com>
2019-03-21 08:53:17 -0400

    Add solve for reserial

---
+++ src/angr/radare2/serial/reverse/solve.py
@@ -0,0 +1,27 @@
+import angr
+
+"""
+Solving reserial - NOT WORKING
+flag: EZAV
+
+```
+python solve.py
+```
+"""
+
+
+def main():
+    proj = angr.Project("reserial")
+
+    state = proj.factory.blank_state(addr=0x100000ea4)
+    simgr = proj.factory.simulation_manager(state)
+    simgr.explore(find=(0x100000f30))
+
+    solution_state = simgr.found[0]
+    print(print(solution_state.posix.dumps(0)))
+    print(solution_state.solver.BVV(0x10, 4))
+    print(solution_state.regs.rbp-0x10)
+
+
+if __name__ == "__main__":
+    main()
