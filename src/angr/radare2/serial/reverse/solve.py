4ffb36a1a9391135b9a00df2e54d5f0c9ebbbd28
sylhare <sylhare@outlook.com>
2020-07-16 10:01:48 -0400

    Update reserial

--- src/angr/radare2/serial/reverse/solve.py
+++ src/angr/radare2/serial/reverse/solve.py
@@ -1,4 +1,5 @@
 import angr
+import sys
 
 """
 Solving reserial - NOT WORKING
@@ -15,10 +16,12 @@ def main():
 
     state = proj.factory.blank_state(addr=0x100000ea4)
     simgr = proj.factory.simulation_manager(state)
+
     simgr.explore(find=0x100000f30)
 
     solution_state = simgr.found[0]
-    print(print(solution_state.posix.dumps(0)))
+    print(solution_state.posix.dumps(sys.stdin.fileno()))
+    print(solution_state.posix.dumps(0))
     print(solution_state.solver.BVV(0x10, 4))
     print(solution_state.regs.rbp-0x10)
 
